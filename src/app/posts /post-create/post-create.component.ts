718323601338d72e9abf30ce90d9eb0bf17f0acc
christopher <christopher26@gmail.com>
2025-02-05 00:49:34 +0800

    Updated file with latest changes

---
+++ src/app/posts /post-create/post-create.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: 'post-create',
+    templateUrl: './post-create.component.html',
+    styleUrls: ['./post-create.component.css'],
+})
+
+export class PostCreateComponent {
+    NewPost = 'This is the initial post';
+    EnterValue = '';
+
+    onAddPost() {
+        this.NewPost = this.EnterValue;
+    }
+}
